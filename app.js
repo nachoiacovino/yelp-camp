8ac65b64d05fb47d8d5319461ec4a8d66f4060cc
naknekv <nacho.iacovino@gmail.com>
2019-10-27 16:08:44 +0000

    Add MongoDB, create Schema and add Description to camps

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express"),
+	  bodyParser = require("body-parser"),
+	  mongoose = require("mongoose"),
+	  app = express()
 
-const app = express()
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
-let campgrounds = [
-		{name: "Salmon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-		{name: "Granite Hill", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-		{name: "Mountain Goat's", image: "https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"}
-	]
+// SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+})
+
+const Campground = mongoose.model("Campground", campgroundSchema)
+
+//Campground.create({
+//	name: "Mountain Goat's", image: "https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+//}, (err, campground) => {
+//	if (err) console.log(err)
+//	else console.log(campground)
+// })
+
+//
 
 app.get("/", (req, res) => {
 	res.render("landing")
 })
 
 app.get("/campgrounds", (req, res) => {
-	res.render("campgrounds", {campgrounds})
+	Campground.find({}, (err, campgrounds) => {
+		if (err) console.log(err)
+		else res.render("campgrounds", {campgrounds})
+	})
+	
 })
 
 app.get("/campgrounds/new", (req, res) => {
@@ -25,8 +44,27 @@ app.get("/campgrounds/new", (req, res) => {
 })
 
 app.post("/campgrounds", (req, res) => {
-	campgrounds.push({name: req.body.name, image: req.body.image})
-	res.redirect("campgrounds")
+	// campgrounds.push({name: req.body.name, image: req.body.image})
+	Campground.create({
+		name: req.body.name, 
+		image: req.body.image,
+		description: req.body.description
+	}, (err, newCamp) => {
+		if (err) console.log(err)
+		else res.redirect("campgrounds")
+	})
+	
+})
+
+app.get("/campgrounds/:id", (req, res) => {
+	// const detailCamp = req.body
+	Campground.find({
+		id: req.params.id
+	}), (err, detailCamp) => {
+		if (err) console.log(err)
+		else res.render("detailCamp", {detailCamp})
+	}
+	
 })
 
 
